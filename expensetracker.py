4161cd2dd29e704fa379cbe998568fa275cc948a
Allan Bramwell <allanbram@hotmail.com>
2024-12-10 13:27:40 +0000

    Added files via upload
    Attempting to use sessions instead of javascript to keep it on task using Flask

--- expensetracker.py
+++ expensetracker.py
@@ -1,65 +1,72 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+import uuid
 
-#! initialize Flask app
-expensetracker=Flask(__name__)
+#! Initialize Flask app
+expensetracker = Flask(__name__)
+expensetracker.secret_key = 'bootcamp2024!'
 
-#! I was using an sql database but now it should be handled by localstorage
-#! Connection to database
-expensetracker.config['MYSQL_HOST']='sql8.freesqldatabase.com'
-expensetracker.config['MYSQL_USER']='sql8750720'
-expensetracker.config['MYSQL_PASSWORD']='JkVZEa4fZY'
-expensetracker.config['MYSQL_DB']='sql8750720'
+#! Connect to sql database
+expensetracker.config['MYSQL_HOST'] = 'sql8.freesqldatabase.com'
+expensetracker.config['MYSQL_USER'] = 'sql8750720'
+expensetracker.config['MYSQL_PASSWORD'] = 'JkVZEa4fZY'
+expensetracker.config['MYSQL_DB'] = 'sql8750720'
 
-#! initialize MySQL Connection
-mysql=MySQL(expensetracker)
+mysql = MySQL(expensetracker)
 
+#! Check for unique user id
+@expensetracker.before_request
+def ensure_user_id():
+    if 'user_id' not in session:
+        session['user_id'] = str(uuid.uuid4())
+        print(f"Assigned user_id: {session['user_id']}")
+
+#! fetch data
 @expensetracker.route('/')
 def index():
-    cur=mysql.connection.cursor()
-    cur.execute('SELECT * FROM expenses')
-    data=cur.fetchall()
-    return render_template('index.html', expenses=data)
-
-
-#! inserting data
-@expensetracker.route('/insert', methods=['POST'])
-def insert():
-    category=request.form['category']
-    amount=request.form['amount']
-    desc=request.form['description']
-    cur=mysql.connection.cursor()
-    cur.execute('INSERT INTO expenses (category, amount, description) VALUES (%s, %s, %s)',(category, amount, desc))
-    mysql.connection.commit()
-    return redirect(url_for('index'))
-
-#! delete data
-@expensetracker.route('/delete/<string:id>', methods=['GET'])
-def delete(id):
-    cur=mysql.connection.cursor()
-    cur.execute('DELETE FROM expenses WHERE ID=%s', [id])
-    mysql.connection.commit()
-    return redirect(url_for('index'))
-
-#! Delete all data
-@expensetracker.route('/deleteall', methods=['GET'])
-def delete_all():
+    user_id = session['user_id']
     cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM expenses')
-    mysql.connection.commit()
-    return redirect(url_for('index')) 
-
-
-#! Sum all amounts
-@expensetracker.route('/SUM')
-def sum():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT SUM(Amount) as Total FROM expenses')
+    cur.execute('SELECT * FROM expenses WHERE user_id = %s', [user_id])
+    data = cur.fetchall()
+    cur.execute('SELECT SUM(amount) as total FROM expenses WHERE user_id = %s', [user_id])
     result = cur.fetchone()
     total = result[0] if result[0] is not None else 0
-    return str(total)
+    return render_template('index.html', expenses=data, total=total)
 
-#! run server
-#! This has to go last to run everything above
-if __name__=="__main__":
+
+#! insert data
+@expensetracker.route('/insert', methods=['POST'])
+def insert():
+    user_id = session['user_id']
+    category = request.form['category']
+    amount = request.form['amount']
+    description = request.form['description']
+    cur = mysql.connection.cursor()
+    cur.execute(
+        'INSERT INTO expenses (category, amount, description, user_id) VALUES (%s, %s, %s, %s)',
+        (category, amount, description, user_id)
+    )
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+#! delete one row
+@expensetracker.route('/delete/<string:id>', methods=['GET'])
+def delete(id):
+    user_id = session['user_id']
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM expenses WHERE id = %s AND user_id = %s', (id, user_id))
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+#! delete all data
+@expensetracker.route('/deleteall', methods=['GET'])
+def delete_all():
+    user_id = session['user_id']
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM expenses WHERE user_id = %s', [user_id])
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+#! Run the app - must go last
+if __name__ == '__main__':
     expensetracker.run(debug=True)
